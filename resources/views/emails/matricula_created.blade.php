259e4f25c0e50e8cde34486d76d1c83a86d9d93f
dronix <Daniel ferrada>
2025-01-16 18:08:09 -0300

    se crea mail

--- resources/views/emails/matricula_created.blade.php
+++ resources/views/emails/matricula_created.blade.php
@@ -29,7 +29,7 @@
     <br>
     <p>Tu código de matrícula es: <strong>{{ $matricula->codigo }}</strong></p>
     <p>Curso al cual te matriculaste: @if ($matricula->curso) <strong>{{ $matricula->curso->codigo }}</strong> @endif</p>
-    <p>Dirección de la Escuela: Andrés Bello N° 1180, Temuco</p>
+    <p>Dirección de la Escuela: Lord cochrane N° 490, Temuco</p>
     <p>Horario de Clases: Lunes a Viernes de 9:00 a 12:00 y de 14:00 a 17:00</p>
     <p>Para más información, no dudes en contactarnos a través de nuestro correo: <strong>prueba@prueba.cl</strong> o nuestro telefono: <strong>217868</strong></p>
     <br>
